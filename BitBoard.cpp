d21fff2f1c8afb2c24ba35dc91b396f403aa39c9
Robert Hölzel <robert.hoelzel@gmx.de>
2020-03-01 15:37:33 +0100

    inline pdep/pext calls => 50% speed boost on linux

--- BitBoard.cpp
+++ BitBoard.cpp
@@ -1,20 +1,9 @@
 #include "BitBoard.hpp"
 
 #include <iostream>
-#include <x86intrin.h>
 
 namespace spezi
 {
-    BitBoard pdep(BitBoard const source, BitBoard const mask)
-    {
-        return _pdep_u64(source, mask);
-    }
-
-    BitBoard pext(BitBoard const source, BitBoard const mask)
-    {
-        return _pext_u64(source, mask);
-    }
-
     void prettyPrint(BitBoard b)
     {      
         std::cout<<"--------\n";
